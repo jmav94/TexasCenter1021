219ee8e11d1b72a2e2ab1c3f40cbd7e786a1e9e2
ArslanButt <arslanbutt@A006-00716.local>
2021-09-18 22:43:48 +0500

    home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,131 @@
 
-  $(document).ready(function(){
-    $(".tab-content").hide();
-    $(".tab1").show();
+//   $(document).ready(function(){
+//     $(".tab-content").hide();
+//     $(".tab1").show();
 
-    $(".map-points").click(function(){
-        var name =  $(this).attr("data-filter");
-        $(".tab-content").not("."+name).hide();
-        $(".tab-content").filter("."+name).fadeIn();
-    });
+//     $(".map-points").click(function(){
+//         var name =  $(this).attr("data-filter");
+//         $(".tab-content").not("."+name).hide();
+//         $(".tab-content").filter("."+name).fadeIn();
+//     });
+// });
+
+
+// new WOW().init();
+
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop:true,
+  autoplay:true,
+  autoplaySpeed:2000,
+  autoplayTimeout:4000,
+  margin:30,
+  dots:false,
+  nav:true,
+  navText: ["<img src='assets/images/arrow.svg'>","<img src='assets/images/arrow.svg'>"]
 });
 
 
-new WOW().init();
+
+
+
+
+var ctx = document.getElementById("myChartOne");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Jan",	"Feb",	"Mar",	"Apr",	"May",	"Jun",	"Jul", "Aug",	"Sep","Oct", "Nov", "Dec"],
+        datasets: [{
+            label: 'Series 1',
+            data: [100,	120,	200,	320,	300,	200,	120,	247,	120, 190, 370, 300],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 2
+          },
+          {
+            label: 'Series 2', // Name the series
+            data: [120,	220,	110,	300,	270,	150,	120,	200,	320, 160, 270, 320],
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgb(75, 192, 192)',
+            borderWidth: 2
+        }]
+    },
+    options: {
+      responsive: true, 
+    }
+});
+
+var ctx = document.getElementById("myChartTwo");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Jan",	"Feb",	"Mar",	"Apr",	"May",	"Jun",	"Jul", "Aug",	"Sep","Oct", "Nov", "Dec"],
+        datasets: [{
+            label: 'Series 1',
+            data: [100,	120,	200,	320,	300,	200,	120,	247,	120, 190, 370, 300],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderWidth: 2
+          },
+          {
+            label: 'Series 2', // Name the series
+            data: [120,	220,	110,	300,	270,	150,	120,	200,	320, 160, 270, 320],
+            borderColor: 'rgb(54, 162, 235)',
+            backgroundColor: 'rgb(54, 162, 235)',
+            borderWidth: 2
+        }]
+    },
+    options: {
+      responsive: true, 
+    }
+});
+
+var ctx = document.getElementById("myChartThree");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Jan",	"Feb",	"Mar",	"Apr",	"May",	"Jun",	"Jul", "Aug",	"Sep","Oct", "Nov", "Dec"],
+        datasets: [{
+            label: 'Series 1',
+            data: [100,	120,	200,	320,	300,	200,	120,	247,	120, 190, 370, 300],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderWidth: 2
+          },
+          {
+            label: 'Series 2', // Name the series
+            data: [120,	220,	110,	300,	270,	150,	120,	200,	320, 160, 270, 320],
+            borderColor: 'rgb(54, 162, 235)',
+            backgroundColor: 'rgb(54, 162, 235)',
+            borderWidth: 2
+        }]
+    },
+    options: {
+      responsive: true, 
+    }
+});
+
+var ctx = document.getElementById("myChartFour");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Jan",	"Feb",	"Mar",	"Apr",	"May",	"Jun",	"Jul", "Aug",	"Sep","Oct", "Nov", "Dec"],
+        datasets: [{
+            label: 'Series 1',
+            data: [100,	120,	200,	320,	300,	200,	120,	247,	120, 190, 370, 300],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 2
+          },
+          {
+            label: 'Series 2', // Name the series
+            data: [120,	220,	110,	300,	270,	150,	120,	200,	320, 160, 270, 320],
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgb(75, 192, 192)',
+            borderWidth: 2
+        }]
+    },
+    options: {
+      responsive: true, 
+    }
+});
