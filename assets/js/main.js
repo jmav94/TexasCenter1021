f79aa7301e4cdc3076f6bc945991cc214984aa2d
Arslan-work <arslan4work@gmail.com>
2021-09-27 01:40:05 +0500

    all pages completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,51 @@ $(document).ready(function(){
     $(activeTab).fadeIn();
     return false;
   });
+
+
+  $('#tabs-nav-m li:first-child').addClass('active');
+  $('.tab-content-m').hide();
+  $('.tab-content-m:first').show();
+  
+  $('#tabs-nav-m li').click(function(){
+    $('#tabs-nav-m li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content-m').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
+
+  $('#tabs-nav-a li:first-child').addClass('active');
+  $('.tab-content-a').hide();
+  $('.tab-content-a:first').show();
+  
+  $('#tabs-nav-a li').click(function(){
+    $('#tabs-nav-a li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content-a').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
+  $('#tabs-nav-c li:first-child').addClass('active');
+  $('.tab-content-c').hide();
+  $('.tab-content-c:first').show();
+  
+  $('#tabs-nav-c li').click(function(){
+    $('#tabs-nav-c li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content-c').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
 })
 
 
